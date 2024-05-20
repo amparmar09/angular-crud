e64bc9378859f4aa28cda573d49a093f5560bfe3
Ashish Parmar <am.parmar097@gmail.com>
2024-05-20 18:32:38 +0530

    some changes in dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatIconModule, NavbarComponent],
+  imports: [MatIconModule, MatMenuModule, MatSidenavModule, MatListModule, RouterOutlet, MatToolbarModule, DashboardComponent, RouterModule, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  currentUser: any
+  menuOpen: boolean = false;
+  sideNav: boolean = true;
+
+  constructor(private router: Router) {
+    this.currentUser = localStorage.getItem('userDetails')
+
+  }
+
   sidenavOpened = false;
 
   onSidenavChange(isOpened: boolean) {
     this.sidenavOpened = isOpened;
+  }
+
+  sideNavToggle() {
+    this.sideNav = !this.sideNav
+  }
+
+
+  logout() {
+    this.router.navigateByUrl('/login')
+    localStorage.clear();
+    this.currentUser = null;
+
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard, LoginGuard } from './guard/auth.guard';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     { path: 'register', canActivate: [LoginGuard], component: RegisterComponent },
     { path: 'login', canActivate: [LoginGuard], component: LoginComponent },
-    { path: '', canActivate: [authGuard], component: NavbarComponent },
+    { path: '', canActivate: [authGuard], component: DashboardComponent },
+    { path: 'about', canActivate: [authGuard], component: AboutComponent },
+    { path: 'contact', canActivate: [authGuard], component: ContactComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
